46b71ca648c93b277ec4880297247508ed1caec8
stefan22 <stefan.emailacct@gmail.com>
2015-08-13 21:03:41 +0100

    adds grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt){
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['jshint']);
 
 };
