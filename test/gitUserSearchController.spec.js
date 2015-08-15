e4f868a3ae7fe50bf0adb94719b03774b11e9f63
stefan22 <stefan.emailacct@gmail.com>
2015-08-15 21:58:01 +0100

    creates gitUserSearchController and passes test

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -7,6 +7,11 @@ describe('GitUserSeachController', function() {
     ctrl = $controller('GitUserSearchController');
   }));
 
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
 
 
 });
