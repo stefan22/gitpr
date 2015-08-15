e9cf0ad3f3d1ea7b5ab409fd0b7e2d1065b34758
stefan22 <stefan.emailacct@gmail.com>
2015-08-15 21:01:48 +0100

    adds controller to test

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,12 @@
+describe('GitUserSeachController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+
+
+
+});
