39a7a3c4edf0d4079803e9af5dfd19267539db99
stefan22 <stefan.emailacct@gmail.com>
2015-08-16 15:43:06 +0100

    first test

--- test/GitUserSearchController.spec.js
+++ test/GitUserSearchController.spec.js
@@ -7,6 +7,11 @@ describe('GitUserSearchController', function() {
     ctrl = $controller('GitUserSearchController');
   }));
 
+  it('initialises with an empty search result and search term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
 
 
 
