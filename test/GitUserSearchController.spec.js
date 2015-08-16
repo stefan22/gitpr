8059a1e895439633833232b07868edbacd491340
stefan22 <stefan.emailacct@gmail.com>
2015-08-16 15:39:55 +0100

    sets the beforeEach and inject to test file

---
+++ test/GitUserSearchController.spec.js
@@ -0,0 +1,13 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+
+
+
+
+});
