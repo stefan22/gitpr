22251ed646a4052096c1c8b64aa1125b4b93b66c
stefan22 <stefan.emailacct@gmail.com>
2015-08-17 21:17:50 +0100

    adds the github api to controller

---
+++ js/gitUserSearchController.js
@@ -0,0 +1,21 @@
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+
+  var self = this;
+
+  var searchResource = $resource('https://api.github.com/search/users');
+
+
+
+  self.doSearch = function() {
+
+    self.searchResult = searchResource.get(
+      {
+        q: self.searchTerm
+      }
+    );
+
+
+  };
+
+
+}]);
