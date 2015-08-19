c749d2ee761d9a8082802202cfec239b6e07db70
stefan22 <stefan.emailacct@gmail.com>
2015-08-19 13:09:35 +0100

    fixes merge with master

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-githubUserSearch.controller('GitUserSearchController', [function() {
-  var self = this;
-  self.searchResult = {
-    "items": [
-      {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-
-
-    ]
-=======
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
   var self = this;
@@ -35,7 +16,7 @@ githubUserSearch.controller('GitUserSearchController', ['$resource', function($r
     );
 
 
->>>>>>> master
+
   };
 
 
